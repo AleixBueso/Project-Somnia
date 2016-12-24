6bf655f98d571cb96a92330005e36c971ccaee86
Aleix Bueso <aleix.bueso@gmail.com>
2016-12-24 11:42:18 +0100

    First Commit

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour {
+
+    Dropdown scene_selector;
+
+	// Use this for initialization
+	void Start () {
+        scene_selector = GameObject.Find("Scene Selector").GetComponent<Dropdown>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartSimulation()
+    {
+        Application.LoadLevel(scene_selector.captionText.text);
+    }
+}

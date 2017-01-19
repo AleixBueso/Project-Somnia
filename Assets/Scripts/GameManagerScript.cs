2863fc3d9cb2ed1f139d17946eac9eb1ae478fca
Aleix Bueso <aleix.bueso@gmail.com>
2017-01-19 14:01:12 +0100

    Num Pad and Background

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,19 +5,30 @@ using UnityEngine.UI;
 public class GameManagerScript : MonoBehaviour {
 
     Dropdown scene_selector;
+    GameObject code_pad;
 
 	// Use this for initialization
 	void Start () {
         scene_selector = GameObject.Find("Scene Selector").GetComponent<Dropdown>();
+        code_pad = GameObject.Find("Code Pad");
+
+
+        code_pad.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
     public void StartSimulation()
     {
         Application.LoadLevel(scene_selector.captionText.text);
+    }
+
+    public void EnableCodePad()
+    {
+        code_pad.SetActive(!code_pad.active);
+
     }
 }
---
+++ Assets/VideoPlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerScript : MonoBehaviour {
+
+    public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+        movie.loop = true;
+        movie.Play();
+	}
+}
